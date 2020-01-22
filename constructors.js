b1f971e449014ecc5818bbb77eb7ad85a31beb91
user <user@users-MacBook-Air.local>
2020-01-22 16:39:52 -0700

    Whit

--- constructors.js
+++ constructors.js
@@ -14,7 +14,10 @@
   The values for the make and model will come from two parameters, make and model (in that order).
 */
 
-// Code here
+function CarFactory(make, model){
+  this.make = make
+  this.model= model
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,7 +36,8 @@ function Employee(name, email, hireDate) {
     Assign the result of the invocation to a variable called bob.
   */
   
-  // Code here
+  let bob =  new Employee('Bob', 'bob@gmail.com', '01-02-98')
+    
   
   ////////// PROBLEM 3 //////////
   
@@ -56,7 +60,13 @@ function Employee(name, email, hireDate) {
       invoking moveCar on the right object (prius vs mustang).
   */
   
-  // Code here
+  function Car(){
+    this.move = 0
+    this.moveCar = function(){
+      this.move += 10
+      return this.move
+    }
+  }
   
   ////////// PROBLEM 4 //////////
   
@@ -74,8 +84,13 @@ function Employee(name, email, hireDate) {
     this.genre = genre;
     this.rating = rating;
   }
+Movie.prototype.changeRating = function (newRating){
+    let average = (this.rating + newRating) /2;
+      this.rating = average
+        return this.rating
   
-  // Code here
+}
+  
   
   ////////// PROBLEM 5 //////////
   
@@ -83,19 +98,48 @@ function Employee(name, email, hireDate) {
   
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
-  // Code here
+  function User(name, age, email, savedPosts){
+    this.name = name
+    this.age = age
+    this.email = email
+    this.savedPosts = savedPosts
+  }
+  User.prototype.addSavedPost = function(id, title, rating){
+    let newPost = {
+    id,
+    title,
+    rating
+    
+    }
+  this.savedPosts.push(newPost)
+  }
   
   ////////// PROBLEM 6 //////////
   
   // You will be using the constructor function you just created in problem 5.
   // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
   
-  // Code here
+  
+  User.prototype.removeSavedPost = function(id) {
+    this.savedPosts = this.savedPosts.filter(element => {
+      return element.id !== id
+    })
+  }
   
   ////////// PROBLEM 7 //////////
   
   // You will continue to use the constructor function you created in problem 5.
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
-  // Code here
+  User.prototype.changePostRating = function(id, newRating){
+    this.savedPosts.forEach(post => {
+      if (post.id === id){
+        post.rating = newRating
+      }
+    })
+  }
+
+  
+
+
   
--- context.js
+++ context.js
@@ -19,7 +19,13 @@
 */
   
 
-//Code Here
+let user = {
+  username: 'Whitney',
+  email: 'whitney@gmail.com',
+  getUsername: function(){
+     return this.username
+  }
+}
 
 
 
@@ -41,7 +47,8 @@ class Animal {
   }
 }
 
-//Code Here
+let animal1 = new Animal('loki', 'dog', 'pizza')
+
 
 
 
@@ -61,7 +68,7 @@ let who = {
   location: 'Belize'
 }
 
-//Code Here
+let whoSaysHi = sayHi.bind(who)
 
 
 
@@ -76,7 +83,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -87,7 +94,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +105,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -116,4 +123,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1;
